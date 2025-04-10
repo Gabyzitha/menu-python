3769e5e365d33f6fc354721a4f081eaaf0f3927d
Gabrielly Soares <gabysoares201@gmail.com>
2025-04-10 14:54:12 -0300

    add \n nas quebras de linhas add cpf e codigo na hora de incluir add dicionario com essas informações

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#Aluna: Gabrielly Soares Alcântara Mota
-#Curso: Análise e desenvolvimento de sistemas
+# Aluna: Gabrielly Soares Alcântara Mota
+# Curso: Análise e desenvolvimento de sistemas
 
 
 # lista de estudantes vazia para coletar informações
@@ -37,15 +37,29 @@ while True:
 
             # se for opção 1, peça para que inclua alunos na lista
             if opcao2 == "1":
-                print("=========== INCLUIR ===========\n")
+                print("=========== INCLUIR ===========")
 
-                # pedir nome ao usuário
-                nome = input("Informe o nome do estudante: ")
+                # pedir código,nome e cpf ao usuário
+                while True:
+                    codigo = int(input("\nInforme o código do estudante: "))
+                    nome = input("Informe o nome do estudante: ")
+                    cpf = input("Informe o CPF do estudante: ")
+
+                    # coloca essas informações em um dicionário
+                    dados = {"código": codigo, "nome": nome, "cpf": cpf}
+                    # coloca esse dicionário na lista_estudantes
+                    lista_estudantes.append(dados)
+                    print("\nEstudante cadastrado com sucesso!\n")
+
+                    if input("Deseja cadastrar um novo contato (s/n)? ") == "n":
+                        break
+
                 print("Pressione ENTER para continuar.\n")
                 print("=" * 30)
 
                 # colocar o "nome" na listaEstudantes
-                lista_estudantes.append(nome)
+                # dicionario vazio
+
 
             # se for opção 2, exiba a lista de alunos
             elif opcao2 == "2":
@@ -57,19 +71,20 @@ while True:
                 # senão, exibir a lista
                 else:
                     print("Lista de Estudantes:\n")
-                    for i, estudante in enumerate(lista_estudantes,start=1):
-                        print(f"{i}. {estudante}")
+                    for i, dado in enumerate(lista_estudantes):
+                        print(f"{i + 1}", dado)
                     print("Pressione ENTER para continuar.\n")
                     print("=" * 29)
 
 
-                    # senão, se for uma dessas opções, mostre que está em desenvolvimento
+            # senão, se for uma dessas opções, mostre que está em desenvolvimento
+
             elif opcao2 == "3" or opcao2 == "4":
                 print(f"Você escolheu a opção válida ({opcao2})\n")
                 # mostrando que está em desenvlvimento
                 print("====== ATUALIZAÇÃO ======\n")
                 print("    EM DESENVOLVIMENTO\n")
-                print("=" *25)
+                print("=" * 25)
 
             # senão, se a opção for = 9, volte para o menu principal
             elif opcao2 == "9":
@@ -83,7 +98,7 @@ while True:
     elif opcao == "2" or opcao == "3" or opcao == "4" or opcao == "5":
         print("====== ATUALIZAÇÃO ======\n")
         print("    EM DESENVOLVIMENTO\n")
-        print("=" * 25 )
+        print("=" * 25)
 
     # senão, se a opção for = 9, volte para o menu principal
     elif opcao == "9":
