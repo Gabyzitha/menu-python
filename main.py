cb7c6000871c0b67d989aed61e1baa33256bb6c3
Gabrielly Soares <gabysoares201@gmail.com>
2025-04-28 11:49:56 -0300

    Foram adicionados as funções para criar arquivos JSON e a leitura dos mesmos, Foi adicionada uma função que inclui todas as funcionalidades do meunu de operações, foram implementadas medidas de exceções

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Aluna: Gabrielly Soares Alcântara Mota
 # Curso: Análise e desenvolvimento de sistemas
 
+import json
 
-#lista de pessoas vazia para coletar informações
+
+# lista de pessoas vazia para coletar informações
 lista_pessoas = []
 
 def em_desenvolvimento():
@@ -10,7 +12,8 @@ def em_desenvolvimento():
     print("\n    EM DESENVOLVIMENTO\n")
     print("=" * 25)
 
-#função para exibir o menu principal
+
+# função para exibir o menu principal
 def mostrar_menu_principal():
     print("----- [MENU PRINCIPAL] -----\n")
     print("(1) Gerenciar estudantes.")
@@ -20,7 +23,8 @@ def mostrar_menu_principal():
     print("(5) Gerenciar matrículas.")
     print("(9) Sair.\n")
 
-#função para exibir o menu de operações
+
+# função para exibir o menu de operações
 def mostrar_menu_operacoes():
     print("(1) Incluir.")
     print("(2) Listar.")
@@ -28,30 +32,45 @@ def mostrar_menu_operacoes():
     print("(4) Excluir.")
     print("(9) Voltar ao menu principal.\n")
 
-#função de incluir código, nome e cpf
-def incluir():
+
+# função de incluir código, nome e cpf
+def incluir(nome_arquivo, lista_pessoas):
     print("============== INCLUIR ==============")
     while True:
-        #pede o código, nome cpf ao usuário
-        codigo = int(input("\nInforme o código: "))
-        nome = input("Informe o nome: ")
-        cpf = input("Informe o CPF: ")
+        # pede o código, nome cpf ao usuário
+       try:
+            codigo = int(input("\nInforme o código: "))
+            nome = input("Informe o nome: ")
+            cpf = input("Informe o CPF: ")
 
-        # coloca essas informações em um dicionário
-        pessoa_cadastrada = {"código": codigo, "nome": nome, "cpf": cpf}
-        # coloca esse dicionário na lista_pessoas
-        lista_pessoas.append(pessoa_cadastrada)
-        print("\n Cadastro feito com sucesso!\n")
+            # coloca essas informações em um dicionário
+            pessoa_cadastrada = {"código": codigo, "nome": nome, "cpf": cpf}
 
-        # se a resposta for = "n" volte para o menu de operações
-        if input("Deseja cadastrar uma nova pessoa (s/n)? ") == "n":
-            break
+            #atualiza a lista lendo o arquivo
+            lista_pessoas = ler_arquivo(nome_arquivo)
+
+            # adicona a pessoa na lista
+            lista_pessoas.append(pessoa_cadastrada)
+
+            # salva a lista atualizada
+            salvar_arquivo(nome_arquivo, lista_pessoas)
+            print("\n Cadastro feito com sucesso!\n")
+
+            # se a resposta for = "n" volte para o menu de operações
+            if input("Deseja cadastrar uma nova pessoa (s/n)? ") == "n":
+                 break
+
+       except ValueError:
+          print(" ✖ Apenas números inteiros são considerados, tente novamente! \n")
+
 
     print("Pressione ENTER para continuar.\n")
     print("=" * 36)
 
-#função para exibir a lista
-def lista():
+
+# função para exibir a lista
+def listar(nome_arquivo):
+    lista_pessoas = ler_arquivo(nome_arquivo)
     print("=========== LISTA ===========\n")
     # se a lista estiver vazia, informe ao usuário
     if len(lista_pessoas) == 0:
@@ -60,65 +79,129 @@ def lista():
     # senão, exibir a lista
     else:
         print("Lista de Cadastros:\n")
-        for i, dados in enumerate(lista_pessoas):
-            print(dados)
-        print("Pressione ENTER para continuar.\n")
-        print("=" * 29)
+        for dados in lista_pessoas:
+            print(f"Código: {dados['código']}")
+            print(f"Nome: {dados['nome']}")
+            print(f"CPF: {dados['cpf']}")
+            print("-" * 30)
 
-#função para editar o cadastro
-def editar():
+
+# função para editar o cadastro
+def editar(nome_arquivo):
+    lista_pessoas = ler_arquivo(nome_arquivo)
     print("=========== EDITAR ===========\n")
 
-    # coletando o código da pessoa no qual o usuário deseja editar
-    codigo_editar = int(input("Informe o código da pessoa que deseja editar: "))
+    try:
 
-    editar_cadastro = None
+        # coletando o código da pessoa no qual o usuário deseja editar
+        codigo_editar = int(input("Informe o código da pessoa que deseja editar: "))
 
-    for pessoa_cadastrada in lista_pessoas:
-        if pessoa_cadastrada["código"] == codigo_editar:
-            editar_cadastro = pessoa_cadastrada
-            print("Por favor, digite as novas informações de cadastro!\n")
-            editar_cadastro["código"] = int(input("\nInforme o novo código: "))
-            editar_cadastro["nome"] = input("Informe o novo nome: ")
-            editar_cadastro["cpf"] = input("Informe o novo CPF: ")
-            print("\nEdição feita com sucesso!\n")
-            break
+        editar_cadastro = None
 
-    if editar_cadastro is None:
-        print(f"Não encontramos a pessoa de código: {codigo_editar}.\n")
-    print("=" * 29)
+        for pessoa_cadastrada in lista_pessoas:
+            if pessoa_cadastrada["código"] == codigo_editar:
+                editar_cadastro = pessoa_cadastrada
+                print("Por favor, digite as novas informações de cadastro!\n")
+                editar_cadastro["código"] = int(input("\nInforme o novo código: "))
+                editar_cadastro["nome"] = input("Informe o novo nome: ")
+                editar_cadastro["cpf"] = input("Informe o novo CPF: ")
+                print("\nEdição feita com sucesso!\n")
+                salvar_arquivo(nome_arquivo, lista_pessoas)
+                break
 
-def excluir():
+        if editar_cadastro is None:
+            print(f"Não encontramos a pessoa de código: {codigo_editar}.\n")
+        print("=" * 29)
+    except ValueError:
+        print(" ✖ Apenas números inteiros são considerados, tente novamente! \n")
+
+
+# função de excluir
+def excluir(nome_arquivo):
+    lista_pessoas = ler_arquivo(nome_arquivo)
     print("=========== EXCLUIR ===========\n")
 
-    # coletando o código da pessoa no qual o usuário deseja excluir
-    codigo_excluir = int(input("Informe o código da pessoa que deseja excluir: "))
+    try:
+        # coletando o código da pessoa no qual o usuário deseja excluir
+        codigo_excluir = int(input("Informe o código da pessoa que deseja excluir: "))
 
-    remover_cadastro = None
-    for pessoa_cadastrada in lista_pessoas:
-        if pessoa_cadastrada["código"] == codigo_excluir:
-            remover_cadastro = pessoa_cadastrada
+        remover_cadastro = None
+        for pessoa_cadastrada in lista_pessoas:
+            if pessoa_cadastrada["código"] == codigo_excluir:
+                remover_cadastro = pessoa_cadastrada
+                break
+
+        if remover_cadastro:
             lista_pessoas.remove(remover_cadastro)
-            print("Cadastro removido com sucesso!\n")
-            break
-        elif remover_cadastro is None:
+            salvar_arquivo(nome_arquivo, lista_pessoas)
+            print("Cadastro Removido com sucesso! \n")
+        else:
             print(f"Não encontramos a pessoa de código: {codigo_excluir}.\n")
-    print("=" * 29)
+
+        print("=" * 29)
+    except ValueError:
+        print(" ✖ Apenas números inteiros são considerados, tente novamente! \n")
 
 
+# função criar arquvivo JSON
+def salvar_arquivo(nome_arquivo, lista_pessoas):
+     with open(nome_arquivo, "w", encoding='utf-8') as arquivo_aberto:
+          json.dump(lista_pessoas, arquivo_aberto, ensure_ascii=False, indent=4)
 
-#mostre o menu principal enquanto o usuário n decidir sair
+
+# função ler arquivo JSON
+def ler_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, "r", encoding='utf-8') as arquivo_aberto:
+            lista_pessoas = json.load(arquivo_aberto)
+
+        return lista_pessoas
+    except:
+        return []
+
+
+# função que abrange todas as funcionalidades do menu de operações
+def funcionalidades_menu_opercoes(nome_arquivo, lista_pessoas, opcao2):
+    # se for opção 1, peça para que inclua alunos na lista
+    if opcao2 == "1":
+        incluir(nome_arquivo, lista_pessoas)
+
+    # se for opção 2, exibir a lista de alunos
+    elif opcao2 == "2":
+        listar(nome_arquivo)
+
+    elif opcao2 == "3":
+        editar(nome_arquivo)
+
+    # se for opção 4, exibir a sessão de excluir
+    elif opcao2 == "4":
+        excluir(nome_arquivo)
+
+    # senão, se a opção for = 9, volte para o menu principal
+    elif opcao2 == "9":
+        print("Você escolheu voltar para o [MENU PRINCIPAL]\n")
+        return False
+    # senão for nenhuma dessas, considere inválida
+    else:
+        print("Você escolheu uma opção INVÁLIDA, tente novamente!\n")
+
+    return True
+
+
+# mostre o menu principal enquanto o usuário n decidir sair
 while True:
     # menu principal
     mostrar_menu_principal()
 
-    # coletando a opção 1
+    # coletando opção 1
     opcao = input("Informe a opção desejada: ")
+    print(f"Você escolheu a opção válida ({opcao})\n")
 
     # se a opção for 1, mostre o menu de operações enquanto o usuário não decidir sair
     if opcao == "1":
+        nome_arquivo = "estudantes.json"
         while True:
-            print(f"Você escolheu a opção válida ({opcao})\n")
+
 
             # menu de operações
             print(f"★★★★★ [ESTUDANTES] MENU DE OPERAÇÕES ★★★★★\n")
@@ -127,29 +210,9 @@ while True:
             # coletando a opção2
             opcao2 = input("Informe a opção desejada: ")
             print(f"Você escolheu a opção válida ({opcao2})\n")
-
-            # se for opção 1, peça para que inclua alunos na lista
-            if opcao2 == "1":
-                incluir()
-
-            # se for opção 2, exibir a lista de alunos
-            elif opcao2 == "2":
-                lista()
-
-            elif opcao2 == "3":
-                editar()
-
-            # se for opção 4, exibir a sessão de excluir
-            elif opcao2 == "4":
-                excluir()
-
-            # senão, se a opção for = 9, volte para o menu principal
-            elif opcao2 == "9":
-                print("Você escolheu voltar para o [MENU PRINCIPAL]\n")
+            continuar = funcionalidades_menu_opercoes(nome_arquivo, lista_pessoas, opcao2)
+            if not continuar:
                 break
-            # senão for nenhuma dessas, considere inválida
-            else:
-                print("Você escolheu uma opção INVÁLIDA, tente novamente!\n")
 
 
 
